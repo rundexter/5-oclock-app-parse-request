b0d7baae8a9ebe5853c75fb853cbf594343f89d0
Justin Fargione <justin@lexicalbits.com>
2016-02-11 17:41:01 -0500

    First working version

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var _ = require('lodash')
+    , assert = require('assert')
+;
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +9,21 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var msg = step.input('text').first()
+            , zip, categories, response
+        ;
+        assert(msg);
+        zip = msg.replace(/.*?([\d]*)$/, '$1');
+        categories = msg.replace(zip, '').split(',');
+        categories = _.filter(categories, function(category) {
+            return category.trim() || false;
+        });
+        if(categories.length > 0) {
+            this.complete(_.map(categories, function(category) {
+                return { zipcode: zip, category: category.trim() };
+            }));
+        } else {
+            this.complete({ zipcode: zip, category: null });
+        }
     }
 };
